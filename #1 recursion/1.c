64252da923b92388d5e99272a1e9cf6129e60474
civito <hyunddol29@naver.com>
2018-01-15 04:43:27 +0900

    문자열 길이계산

---
+++ #1 recursion/1.c
@@ -0,0 +1,19 @@
+/*#include<stdio.h>
+#define MAX 1000
+
+int length(char* str) {
+	if (*str == '\0') {
+		return 0;
+	}
+	else {
+		return 1 + length(str + 1);
+	}
+}
+
+int main() {
+	char str[MAX];
+
+	scanf("%s", str);
+
+	printf("%d", length(str));
+}*/
